ef773418e15b368129d4c2292513c04c290d4f75
sneyed <sneyed@gmail.com>
2019-06-17 00:11:55 +1000

    switching to material ui

--- components/work.js
+++ components/work.js
@@ -1,38 +1,58 @@
 import Lorum from "./lorum";
 import { Fragment } from "react";
 import GalleryRow from "./galleryRow";
+import dynamic from "next/dynamic";
 
-const gallery = [
-  {
-    items: [
-      { title: "Cat1", url: "http://placekitten.com/400/500", width: 4 },
-      { title: "Cat2", url: "http://placekitten.com/400/500", width: 4 },
-      { title: "Cat3", url: "http://placekitten.com/400/500", width: 4 }
-    ]
-  },
-  {
-    height: 200,
-    items: [
-      { title: "Cat4", url: "http://placekitten.com/600/200", width: 6 },
-      { title: "Cat5", url: "http://placekitten.com/600/200", width: 6 }
-    ]
-  },
-  {
-    height: 400,
-    items: [
-      { title: "Cat6", url: "http://placekitten.com/900/400", width: 11 },
-      { title: "Cat7", url: "http://placekitten.com/100/400", width: 1 }
-    ]
-  }
+const Muuri = dynamic(() => import("./muuri"), {
+  ssr: false
+});
+
+// const gallery = [
+//   {
+//     items: [
+//       { title: "Cat1", url: "http://placekitten.com/400/500", width: 4 },
+//       { title: "Cat2", url: "http://placekitten.com/400/500", width: 4 },
+//       { title: "Cat3", url: "http://placekitten.com/400/500", width: 4 }
+//     ]
+//   },
+//   {
+//     height: 200,
+//     items: [
+//       { title: "Cat4", url: "http://placekitten.com/600/200", width: 6 },
+//       { title: "Cat5", url: "http://placekitten.com/600/200", width: 6 }
+//     ]
+//   },
+//   {
+//     height: 400,
+//     items: [
+//       { title: "Cat6", url: "http://placekitten.com/900/400", width: 11 },
+//       { title: "Cat7", url: "http://placekitten.com/100/400", width: 1 }
+//     ]
+//   }
+// ];
+
+const muuriGallery = [
+  { title: "Cat1", url: "http://placekitten.com/500/500" },
+  { title: "Cat2", url: "http://placekitten.com/500/500" },
+  { title: "Cat3", url: "http://placekitten.com/500/500" }
 ];
 
 export default props => (
-  <div>
-    {gallery.map((row, index) => {
-      return (
-        <GalleryRow key={index} items={row.items} height={row.height || 500} />
-      );
-    })}
-    <Lorum />
-  </div>
+  <Fragment>
+    <div>
+      <Muuri items={muuriGallery} />
+    </div>
+    <style jsx>{``}</style>
+  </Fragment>
 );
+
+// <Lorum />
+// {gallery.map((row, index) => {
+//   return (
+//     <GalleryRow
+//       key={index}
+//       items={row.items}
+//       height={row.height || 500}
+//     />
+//   );
+// })}
